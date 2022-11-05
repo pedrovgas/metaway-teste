dd0d0f1a94e9935e02e21b493cc182c629dbb68f
Pedro Vargas <pedrofreitasvgas@gmail.com>
2022-11-05 12:33:59 -0300

    change project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <span class="material-icons">
+      menu
+    </span>
+    <router-link to="/">Home</router-link>
+    <router-link to="/">Teste</router-link>
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
+
 </template>
 
-<style>
-#app {
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 nav {
-  padding: 30px;
+  border-radius: 320px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 75px;
+  position: fixed;
+  background-color: #2c3e50;
+  width: 75px;
+  overflow: hidden;
+  margin: auto;
+  bottom: 0px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transition: width 200ms;
+  transition-timing-function: ease-in-out;
+  transition: 500ms;
+  opacity: .5;
+
+  &:hover {
+    width: 15vw;
+    opacity: 1;
+  }
+
+  &:hover a {
+    display: block;
+  }
+
+  &:hover span{
+    display: none;
+  }
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  transition: 500ms;
+  color: white;
+  text-decoration: none;
+  display:none
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+nav span.material-icons{
+  color: white;
+  font-size: 30px;
 }
 </style>
